d580bb2441173066f08537eca8aad7ff9542a39c
Peter Amstutz <peter.amstutz@curoverse.com>
2014-01-10 16:19:55 -0500

    Download put into main module

---
+++ download.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import pgp_survey
+
+pgp_survey.download_surveys()
--- pgp_survey.py
+++ pgp_survey.py
@@ -176,3 +176,35 @@ def get_traits_for(huID, participant_survey, trait_surveys, trait_survey_traits)
                 
     return hutraits
     
+import urllib2
+from datetime import datetime
+
+def download_surveys():
+    surveys = { 8: "Blood",
+                6: "Cancers",
+                11: "CirculatorySystem",
+                17: "CongenitalTraitsAndAnomalies",
+                13: "DigestiveSystem",
+                7: "Endocrine,Metabolic,Nutritional,AndImmunity",
+                14: "GenitourinarySystems",
+                16: "MusculoskeletalSystemAndConnectiveTissue",
+                9: "NervousSystem",
+                12: "RespiratorySystem",
+                15: "SkinAndSubcutaneousTissue",
+                10: "VisionAndHearing"
+                }
+
+    s1 = urllib2.urlopen("https://my.personalgenomes.org/google_surveys/1/download")
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    f = open("PGPParticipantSurvey-{0}.csv".format(now), 'w')
+    f.write(s1.read())
+    f.close()
+    s1.close()
+
+    for n, t in surveys.items():
+        s1 = urllib2.urlopen("https://my.personalgenomes.org/google_surveys/{0}/download".format(n))
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        f = open("PGPTrait&DiseaseSurvey2012-{1}-{0}.csv".format(now, t), 'w')
+        f.write(s1.read())
+        f.close()
+        s1.close()
