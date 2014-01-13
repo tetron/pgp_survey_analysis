d0161eae5db21dd3cf26ae2182d13341e93d29aa
Peter Amstutz <peter.amstutz@curoverse.com>
2014-01-13 16:38:31 -0500

    moved trait links query outside of main loop

--- arvados_import.py
+++ arvados_import.py
@@ -23,12 +23,14 @@ huIDs = list(participant_survey.get_huIDs())
 
 api = arvados.api('v1')
 
-print("Initializing from database")
+print("Getting traits list from database")
 
 traits_query = api.traits().list(limit=100000).execute()["items"]
 
 for a in traits_query:
     all_traits[a["name"]] = a
+
+print("Getting humans list from database")
 
 humans_query = api.links().list(limit=100000, where={"head_kind":"arvados#human", "link_class": "identifier", "name": huIDs}).execute()["items"]
 humans = {}
@@ -38,6 +40,23 @@ for a in humans_query:
 survey_columns = {}
 for i in xrange(0, len(participant_survey.header_titles)):
     survey_columns[participant_survey.header_titles[i]] = i
+
+human_uuids = [humans[h]["head_uuid"] for h in humans]
+
+print("Getting humans/trait links from database")
+
+hutraits_query = api.links().list(limit=100000,
+                                  where={"name": "pgp-survey-response",
+                                         "link_class": "human_trait",
+                                         "tail_uuid": human_uuids}).execute()["items"]
+
+hutraits_db = {}
+for t in hutraits_query:
+    if t["tail_uuid"] not in hutraits_db:
+        hutraits_db[t["tail_uuid"]] = []
+    hutraits_db[t["tail_uuid"]].append(t)
+
+#pprint.pprint(hutraits_db)
 
 ct = 0
 for huID in huIDs:
@@ -57,19 +76,19 @@ for huID in huIDs:
     #    if len(participant_survey.get_latest_responses(huID)[i].strip()) > 0:
     #        print("{2}) {0}: {1}".format(participant_survey.header_titles[i], participant_survey.get_latest_responses(huID)[i], i))
 
-    hutraits_query = api.links().list(where={"name": "pgp-survey-response",
-                                             "link_class": "human_trait",
-                                             "tail_uuid": humans[huID]["head_uuid"]}).execute()["items"]
-
     hutraits = pgp_survey.get_traits_for(huID, participant_survey, trait_surveys, trait_survey_traits)
 
-    already_added_traits = [t["head_uuid"] for t in hutraits_query]
+    already_added_traits = []
+    if humans[huID]["head_uuid"] in hutraits_db:
+        already_added_traits = [t["head_uuid"] for t in hutraits_db[humans[huID]["head_uuid"]]]
+        #pprint.pprint(already_added_traits )
 
     for ht in hutraits:
         if ht not in all_traits or all_traits[ht] == None:
             print("Creating trait {0}".format(ht))
             all_traits[ht] = api.traits().create(body={"trait": {"name": ht}}).execute()
 
+        #pprint.pprint(all_traits[ht])
         if all_traits[ht]["uuid"] not in already_added_traits:
             print("{0}/{2}: Adding trait '{1}'".format(huID, ht, humans[huID]["head_uuid"]))
 
